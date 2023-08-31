ce29e74d4d24fe9c809f8b1e34fb03a01aec7955
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-31 20:17:21 -0300

    feat: add dashoboard info and link with localStorage

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,82 +1,63 @@
 <template>
-  <v-container class="dashboard">
-    <h1>Bem-vindo, {{ name }}</h1>
-
-    <v-row justify="center">
-      <v-col cols="12" md="6">
-        <v-card
-          class="mx-auto mt-10"
-          max-width="600"
-          elevation="10"
-          color="black"
-        >
-          <v-card-title class="mt-4">Alunos Cadastrados</v-card-title>
-          <v-card-text class="dashboard-card-content">{{
-            students
-          }}</v-card-text>
-          <v-card-actions>
-            <v-btn color="blue" @click="$router.push('/cadastro-aluno')"
-              >Adicionar</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card
-          class="mx-auto mt-10"
-          max-width="600"
-          elevation="10"
-          color="black"
-        >
-          <v-card-title class="mt-4">Exercícios</v-card-title>
-          <v-card-text class="dashboard-card-content">{{
-            exercises
-          }}</v-card-text>
-          <v-card-actions>
-            <v-btn color="blue" @click="$router.push('/cadastro-treino')"
-              >Adicionar</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
-
-<script>
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      name: "",
-      students: 0,
-      exercises: 0,
-    };
-  },
-  mounted() {
-    this.fetchDashboardData();
-  },
-  methods: {
-    async fetchDashboardData() {
-      try {
-        const response = await axios.get("http://localhost:3000/dashboard");
-        const data = response.data;
-
-        this.name = data.name;
-        this.students = data.students;
-        this.exercises = data.exercises;
-      } catch (error) {
-        console.error("Erro ao buscar dados do dashboard:", error);
-      }
+    <v-container class="dashboard">
+      <h1>Bem-vindo, {{ dashboardInfo }}</h1>
+  
+      <v-row justify="center">
+        <v-col cols="12" md="6">
+          <v-card
+            class="mx-auto mt-10"
+            max-width="600"
+            elevation="10"
+            color="black"
+          >
+            <v-card-title class="mt-4">Alunos Cadastrados</v-card-title>
+            <v-card-text class="dashboard-card-content">{{ students }}</v-card-text>
+            <v-card-actions>
+              <v-btn color="blue" @click="$router.push('/cadastro-aluno')">Adicionar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card
+            class="mx-auto mt-10"
+            max-width="600"
+            elevation="10"
+            color="black"
+          >
+            <v-card-title class="mt-4">Exercícios</v-card-title>
+            <v-card-text class="dashboard-card-content">{{ exercises }}</v-card-text>
+            <v-card-actions>
+              <v-btn color="blue" @click="$router.push('/cadastro-treino')">Adicionar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        dashboardInfo: {},
+        userInfo: {}
+      };
     },
-  },
-};
-</script>
+    mounted() {
+      axios.get("http://localhost:3000/dashboard")
+      .then(res => this.dashboardInfo = res.data)
+      .catch(error => console.log(error))
 
-<style scoped>
-.dashboard-card-content {
-  font-size: 24px;
-  font-weight: bold;
-}
-</style>
+      this.userInfo = JSON.parse(localStorage.getItem("user-info"))  || null
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .dashboard-card-content {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  </style>
