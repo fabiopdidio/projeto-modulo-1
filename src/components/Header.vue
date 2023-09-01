438fe12316a35d7edfa36659b73c95556da73df0
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-09-01 20:17:10 -0300

    :feat: edit header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,32 @@
 <template>
   <header>
-    <v-card
-    color="grey-lighten-4"
-    flat
-    height="200px"
-    rounded="0"
-  >
-    <v-toolbar
-      prominent
-      extended
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-card color="grey-lighten-4" flat height="200px" rounded="0">
+      <v-toolbar prominent extended>
+        <svg-icon type="mdi" :path="path"></svg-icon>
 
-      <v-toolbar-title>TrainSys</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+        <v-toolbar-title>TrainSys</v-toolbar-title>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+        <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
   </header>
 </template>
 
 <script>
+
 export default {
   name: "Header",
 };
